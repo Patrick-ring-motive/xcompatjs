6d81dc7c36ff3dd76f5b3460b0a816a487d8c1b0
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-11-20 20:31:45 +0000

    queueMicrotask

--- .build/lib/IdleCallback/IdleCallback.js
+++ .build/lib/IdleCallback/IdleCallback.js
@@ -1,15 +1,25 @@
 var wandow = window || self || exports;
+wandow.clearStub = function() {
+  return;
+};
 function doSoon(fun) {
-  return setTimeout(fun, 10);
+  if (wandow.setTimeout) {
+    return wandow.setTimeout(fun, 0);
+  } else if (wandow.queueMicrotask) {
+    return wandow.queueMicrotask(fun);
+  } else {
+    return fun();
+  }
 }
 wandow.setImpendingCallback = doSoon;
 wandow.requestAnimationFrame = wandow.requestAnimationFrame || wandow.mozRequestAnimationFrame || wandow.webkitRequestAnimationFrame || wandow.msRequestAnimationFrame;
-wandow.requestIdleCallback || wandow.setImpendingCallback;
+wandow.requestIdleCallback || wandow.setImpendingCallback || wandow.queueMicrotask;
 wandow.mozRequestAnimationFrame = wandow.requestAnimationFrame;
 wandow.webkitRequestAnimationFrame = wandow.requestAnimationFrame;
 wandow.msRequestAnimationFrame = wandow.requestAnimationFrame;
-wandow.cancelAnimationFrame = wandow.cancelAnimationFrame || wandow.mozCancelAnimationFrame || wandow.cancelIdleCallback || wandow.clearTimeout;
+wandow.cancelAnimationFrame = wandow.cancelAnimationFrame || wandow.mozCancelAnimationFrame || wandow.cancelIdleCallback || wandow.clearTimeout || wandow.clearStub;
 wandow.mozCancelAnimationFrame = wandow.cancelAnimationFrame;
 wandow.requestIdleCallback = wandow.requestIdleCallback || wandow.requestAnimationFrame;
-wandow.cancelIdleCallback = wandow.cancelIdleCallback || wandow.cancelAnimationFrame || wandow.clearTimeout;
+wandow.cancelIdleCallback = wandow.cancelIdleCallback || wandow.cancelAnimationFrame || wandow.clearTimeout || wandow.clearStub;
+wandow.queueMicrotask = wandow.queueMicrotask || wandow.requestIdleCallback;
 //# sourceMappingURL=IdleCallback.js.map
